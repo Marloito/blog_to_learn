0fa00ffd32dca8cea135193e36045d640bbe297a
Marlo Ito <email": null>
2017-10-15 16:28:34 -0700

    adds methods in Posts controller for all restful routes w/notes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,32 @@ class PostsController < ApplicationController
   def index
     @posts = Post.all
   end
+
+  def new
+    # get form
+  end
+
+  def create
+    # post action
+  end
+
+  def show
+    # show individual
+  end
+
+  def edit
+    # get form
+  end
+
+  def update
+    # post action
+  end
+
+  def delete
+    # GET form
+  end
+
+  def destroy
+    # post action
+  end
 end
