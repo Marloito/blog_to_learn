2eb85cfa3db0e8e7483b74455668bcce98c1d9b0
Marlo Ito <email": null>
2017-10-15 17:17:41 -0700

    finishes show and creation routes -- links back to HP

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,6 +14,7 @@ class PostsController < ApplicationController
     if @post.save
       redirect_to post_path(@post)
     else
+      @errors = @post.errors.full_messages
       render 'new'
     end
   end
